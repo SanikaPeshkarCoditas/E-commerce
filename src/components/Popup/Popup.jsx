72c023a5db364cb47f54ab88aade7b1afbcad312
sanika <sanika.peshkar@coditas.com>
2024-10-17 16:34:39 +0530

    feat : [POPUP] : Added Popup styling

--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -3,7 +3,7 @@ import styles from './popup.module.css'
 function Popup({children,onClose}) {
   return (
     <div className={styles.PopupContainer}>
-        <span onClick={()=>onClose()}>X</span>
+        <span onClick={()=>onClose()} className={styles.CloseButton}>X</span>
         {children}
     </div>
   )
