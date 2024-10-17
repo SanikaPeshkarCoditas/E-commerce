65ac4d52bc1a93a490394c97651702b61eb6acc9
sanika <sanika.peshkar@coditas.com>
2024-10-17 16:39:47 +0530

    feat: [ ADDTOCART] : added addtoCart functionality

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./productCard.module.css";
-function ProductCard({ productEach, handleProductView }) {
+function ProductCard({ productEach, handleProductView,handleAddtoCart }) {
   const { id, title, description, price } = productEach;
   return (
     <div className={styles.ProductCard} onClick={() => handleProductView(id)}>
@@ -10,6 +10,7 @@ function ProductCard({ productEach, handleProductView }) {
       <h2>{title}</h2>
       <p>{description}</p>
       <span>$ {price}</span>
+      <button onClick={()=>handleAddtoCart()}> Add to Cart</button>
     </div>
   );
 }
